e0c46e02acc9a0b053c8f3a7395a44317bbc3d54
shivangigoel1302 <shivangigoel1301@gmail.com>
2020-06-28 00:10:05 +0530

    greedy1

---
+++ greedy/busy_man.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool compare(pair<int,int>p1,pair<int,int>p2)
+{
+    return p1.second<p2.second;
+}
+int main() {
+   int t,n,s,e;
+   cin>>t;
+   while(t>0)
+   {
+       cin>>n;
+       vector<pair<int,int>>v;
+      for(int i=0;i<n;i++)
+       {
+           cin>>s>>e;
+           v.push_back(make_pair(s,e));
+
+       }
+       sort(v.begin(),v.end(),compare);
+       int res=1;
+       int fin=v[0].second;
+       for(int i=1;i<n;i++)
+       {
+
+           if(v[i].first>=fin)
+           {
+               res++;
+               fin=v[i].second;
+           }
+       }
+       cout<<res<<" ";
+       t--;
+       v.clear();
+   }
+}
